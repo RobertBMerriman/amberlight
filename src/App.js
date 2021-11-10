fdf840d25013520824bfd7af2c8da50c7470c6f2
Robert Merriman <robertbmerriman@gmail.com>
2021-11-10 16:39:34 +0000

    Amberlight holding page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="constrain">
+      <img class="logo" src="sig-initial.png" alt="Amberlight" />
+      <h1>New website under construction</h1>
+      <p class="name">Phillip Attenborough</p>
+      <div class="details-block">
+        <p class="details">phil@amber-light.co.uk</p>
+        <p class="details">+44 792 003 8616</p>
+      </div>
     </div>
   );
 }
